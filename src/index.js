494ce20a1c95e355f4bfc8ac7c8713afba943be0
Wiktoria Skibińska <wikas2000@icloud.com>
2023-12-11 16:27:36 +0100

    styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import axios from "axios";
 import { fetchBreeds,fetchCatByBreed } from "./cat-api";
 import Notiflix from 'notiflix';
-
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 
 
 //selecting html elements
 const body = document.querySelector('body')
-const select = document.querySelector('.breed-select');
+const selectInput = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const loadError = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 loadError.style.display = 'none'
-select.style.display='none'
+selectInput.style.display='none'
 loader.style.display = 'block';
+catInfo.style.display = 'none';
 
 
 let storedBreeds = [];
@@ -22,17 +24,28 @@ let storedBreeds = [];
     fetchBreeds().then(
     
         data => {
-            select.style.display='block'
+            selectInput.style.display='block'
         loader.style.display='none'
-        storedBreeds = data;
+            storedBreeds = data;
+    
         for (let i = 0; i < storedBreeds.length; i++) {
             const breed = storedBreeds[i];
             let option = document.createElement('option');
             option.value = breed.id;
             option.innerHTML = `${breed.name}`;
-            select.appendChild(option);
+            selectInput.appendChild(option);
+          
+
     
-        };
+            };
+              new SlimSelect({
+                  select: selectInput,
+                  settings: {
+                      showSearch: false,
+                  }
+                
+                  
+})
         },).catch(error => {
             Notiflix.Report.failure(
                 'Error!',
@@ -44,11 +57,12 @@ let storedBreeds = [];
 
 
 //
-select.addEventListener("change", event => {
+selectInput.addEventListener("change", event => {
     loader.style.display = 'block';
     catInfo.style.display = 'none';
     let breedId = event.currentTarget.value;
     console.log(breedId);
+    
    
     fetchCatByBreed(breedId).then(
         data => {
@@ -56,9 +70,11 @@ select.addEventListener("change", event => {
             console.log
             console.log(data);
             catInfo.innerHTML = `
-            <img src="${data[0].url}" width="600"></img>
+            <div  class="cat-img"> <img src="${data[0].url}" width="600"></img> </div>
+            <div class="cat-text" style="display: flex">
             <h2> ${data[0].breeds[0].name}</h2>
-            <p>${data[0].breeds[0].description}</p>`  
+            <p>${data[0].breeds[0].description}</p>
+            </div>`  
             catInfo.style.display = 'block'
             
 }
@@ -73,6 +89,9 @@ select.addEventListener("change", event => {
         loader.style.display = 'none'
     })
 });
+
+
+
 
 
 
