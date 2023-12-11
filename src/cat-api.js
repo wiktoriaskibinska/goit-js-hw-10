e9691090884233405450c25958edb9f4e21b99c2
Wiktoria Skibińska <wikas2000@icloud.com>
2023-12-11 23:36:26 +0100

    code formatting and styling

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,16 +1,15 @@
-import axios from "axios";
-const api_key = "live_vkNtmZ1cQWwjQ6PiUVYPeOdlaz9apqiTpT5ThAPyHg65feotuYqLfAC5ra6lJtYy"
+import axios from 'axios';
+const api_key =
+  'live_vkNtmZ1cQWwjQ6PiUVYPeOdlaz9apqiTpT5ThAPyHg65feotuYqLfAC5ra6lJtYy';
 
-
-axios.defaults.headers.common["x-api-key"] = api_key;
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1'
-
+axios.defaults.headers.common['x-api-key'] = api_key;
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
-    return axios.get('/breeds').then(({ data }) => data);
+  return axios.get('/breeds').then(({ data }) => data);
 }
-
 export function fetchCatByBreed(breedId) {
-    return axios.get(`/images/search?breed_ids=${breedId}`).then(({ data}) => data);
+  return axios
+    .get(`/images/search?breed_ids=${breedId}`)
+    .then(({ data }) => data);
 }
-
--- src/index.js
+++ src/index.js
@@ -1,92 +1,74 @@
-import axios from "axios";
-import { fetchBreeds,fetchCatByBreed } from "./cat-api";
+import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-
-
-
 //selecting html elements
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const selectInput = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const loadError = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
-loadError.style.display = 'none'
-selectInput.style.display='none'
+//
+loadError.style.display = 'none';
+selectInput.style.display = 'none';
 loader.style.display = 'block';
 catInfo.style.display = 'none';
-
-
+//
 let storedBreeds = [];
 //
+fetchBreeds()
+  .then(data => {
+    selectInput.style.display = 'flex';
+    loader.style.display = 'none';
+    storedBreeds = data;
 
-    fetchBreeds().then(
-    
-        data => {
-            selectInput.style.display='flex'
-        loader.style.display='none'
-            storedBreeds = data;
-    
-        for (let i = 0; i < storedBreeds.length; i++) {
-            const breed = storedBreeds[i];
-            let option = document.createElement('option');
-            option.value = breed.id;
-            option.innerHTML = `${breed.name}`;
-            selectInput.appendChild(option);
-          
+    for (let i = 0; i < storedBreeds.length; i++) {
+      const breed = storedBreeds[i];
+      let option = document.createElement('option');
+      option.value = breed.id;
+      option.innerHTML = `${breed.name}`;
 
-    
-            };
-            new SlimSelect({
-                select: selectInput,
-            });
-        },).catch(error => {
-            Notiflix.Report.failure(
-                'Error!',
-                'Something went wrong!Try reloading the page!'
-
-            )
-        }
-    )
-
+      selectInput.appendChild(option);
+    }
+    new SlimSelect({
+      select: selectInput,
+    });
+  })
+  .catch(error => {
+    Notiflix.Report.failure(
+      'Error!',
+      'Something went wrong!Try reloading the page!'
+    );
+  });
 
 //
-selectInput.addEventListener("change", event => {
-    loader.style.display = 'flex';
-    catInfo.style.display = 'none';
-    let breedId = event.currentTarget.value;
-    console.log(breedId);
-    
-   
-    fetchCatByBreed(breedId).then(
-        data => {
-            const url = data[0].url;
-            console.log
-            console.log(data);
-            catInfo.innerHTML = `
+selectInput.addEventListener('change', event => {
+  loader.style.display = 'flex';
+  catInfo.style.display = 'none';
+  let breedId = event.currentTarget.value;
+  console.log(breedId);
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      const url = data[0].url;
+      console.log;
+      console.log(data);
+      catInfo.innerHTML = `
             <div  class="cat-img"> <img src="${data[0].url}" width="600"></img> </div>
             <div class="cat-text" style="display: flex">
             <h2> ${data[0].breeds[0].name}</h2>
             <p>${data[0].breeds[0].description}</p>
-            </div>`  
-            catInfo.style.display = 'flex'
-            
-}
-    ).catch(error => {
-            Notiflix.Report.failure(
-                'Error!',
-                'Something went wrong!Try reloading the page!'
-
-            )
-        }
-    ).finally(() => {
-        loader.style.display = 'none'
+            </div>`;
+      catInfo.style.display = 'flex';
     })
+    .catch(error => {
+      Notiflix.Report.failure(
+        'Error!',
+        'Something went wrong!Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      loader.style.display = 'none';
+    });
 });
-
-
-
-
-
-
